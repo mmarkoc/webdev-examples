a32f5cc0e1696d8522b4acf7889eae2e9e82a7f9
Marko Markoc <mmarkoc@pdx.edu>
2021-11-12 15:16:47 -0800

    javascript fetch and functional exercise

--- javascript/exercises/04-dom.js
+++ javascript/exercises/04-dom.js
@@ -2,16 +2,29 @@
 // when the user clicks the 'copy' button, copy the user input to the output area
 
 // fetch JavaScript objects representing specific elements in the DOM
-
+var input = document.getElementById("userInput1");
+var output = document.getElementById("output");
+var button = document.getElementById("copy");
 // add an event listener on the target element
+button.addEventListener("click", buttonClick);
 
 // callback function to handle event
-
+function buttonClick(event) {
+  console.log("click event", event);
+  output.textContent = input.value;
+}
 // Exercise #2:
 // when the user enters input text, copy the user input to the output area
 
 // fetch JavaScript objects representing specific elements in the DOM
-
+let input2 = document.getElementById("userInput2");
+let output2 = document.createElement("div");
 // add an event listener on the target element
+input2.addEventListener("input", buttonEnter);
 
 // callback function to handle event
+function buttonEnter(event) {
+  console.log("click enter", event);
+  output2.textContent = input2.value;
+  document.body.append(output2);
+}
--- javascript/exercises/06-fetch.js
+++ javascript/exercises/06-fetch.js
@@ -1,9 +1,35 @@
-const url = 'https://anapioficeandfire.com/api/books/';
+const url = "https://anapioficeandfire.com/api/books/";
 
-const app = document.querySelector('#books');
+const app = document.querySelector("#books");
 
 const fetchData = (url) => {
   // Fetch all books from the API of Ice and Fire and append them to the DOM
   // Create an element for each book that contains title, author, publication year, and number of pages
   // Update the styles in JavaScript to center all the books in the container given
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+
+      data.forEach((item) => {
+        let book = document.createElement("h2");
+        let authors = document.createElement("p");
+        let year = document.createElement("p");
+        let pages = document.createElement("p");
+
+        book.textContent = item.name;
+        authors.textContent = `by ${item.authors}`;
+        year.textContent = item.released.substring(0, 4);
+        pages.textContent = `${item.numberOfPages} pages`;
+
+        app.append(book);
+        app.append(authors);
+        app.append(year);
+        app.append(pages);
+      });
+    })
+    .catch((error) => console.log("Request Failed", error))
+    .finally(() => app.removeChild(loading));
 };
+
+fetchData(url);
